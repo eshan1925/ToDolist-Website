3de09e7614c5487952965209b4f2bf7d281d5f9d
eshan1925 <eshan1925@gmail.com>
2021-12-12 13:24:54 +0530

    functioning of todo list implemented

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.set('view engine','ejs');
-
+var items = ["Buy Food","Cook Food","Eat Food"];
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function (req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day="";
-    var week = {"Monday":1,"Tuesday":2,"Wednesday":3,"Thursday":4,"Friday":5,"Saturday":6,"Sunday":0};
-    day=Object.keys(week).find(key=>week[key]===currentDay);
-    res.render("list",{kindOfDay:day});
+    var options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+
+    var day = today.toLocaleDateString("en-US",options);
+    // var currentDay = today.getDay();
+    // var day = "";
+    // var week = { "Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6, "Sunday": 0 };
+    // day = Object.keys(week).find(key => week[key] === currentDay);
+    res.render("list", { kindOfDay: day, newListItems:items });
+});
+
+app.post("/",function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+    console.log(item);
 });
 
 app.listen(3000, function () {
