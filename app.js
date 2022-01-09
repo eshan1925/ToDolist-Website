5c119942f7300501306e04bf33f34fcbb07c6f8e
eshan1925 <eshan1925@gmail.com>
2022-01-10 01:27:06 +0530

    Added Deploying Application files

--- app.js
+++ app.js
@@ -153,7 +153,12 @@ app.post("/delete", function (req, res) {
     }
 });
 
+let port = process.env.PORT;
+if(port==null || port==""){
+    port=3000;
+}
+
 //Listen Functions
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(port, function () {
+    console.log("Server has started successfully!");
 });
