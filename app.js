1b51fa335010cf692e6f2a5830088bbb0104e7f7
eshan1925 <eshan1925@gmail.com>
2022-01-09 23:55:20 +0530

    list functionality and db connection done

--- app.js
+++ app.js
@@ -1,52 +1,156 @@
 //required packages
 var express = require('express');
 var bodyParser = require('body-parser');
-var date = require(__dirname+"/date.js");
-
+var date = require(__dirname + "/date.js");
+var mongoose = require("mongoose");
+var _ = require("lodash");
 //app building
 var app = express();
 
-//list of To-do's
-var items = ["Buy Food","Cook Food","Eat Food"];
-let workItems = [];
 
 //Loading Ejs
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//connecting or creating of database
+mongoose.connect("mongodb://localhost:27017/todoListDB", { useNewUrlParser: true });
+
+//defining the schema
+var itemsSchema = {
+    name: String
+};
+
+//creating a collection using the schema
+var Item = mongoose.model("Item", itemsSchema);
+
+//using the schema and building some default objects
+var item1 = new Item({
+    name: "Welcome to your todo List!"
+});
+
+var item2 = new Item({
+    name: "Hit the + button to add a new task!"
+});
+
+var item3 = new Item({
+    name: "<---Hit this to delete an item."
+});
+
+var defaultItems = [item1, item2, item3];
+
+var listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+var List = mongoose.model("List", listSchema);
+// inserting items
+// Item.insertMany(defaultItems, function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully added items!!!");
+//     }
+// });
+
+var day = date.getDate();
 //Get functions
 app.get("/", function (req, res) {
-    let day = date.getDate();
-    res.render("list", { listTitle: day, newListItems:items,buttonName:"Work List🏢",redirectLocation:"http://localhost:3000/work"});
+    //finding items
+    Item.find({}, function (err, foundItems) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundItems.length === 0) {
+                Item.insertMany(defaultItems, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Successfully added items!!!");
+                    }
+                });
+                res.redirect("/"); //for redirecting so that we can see the items
+            } else {
+                console.log("Successfully found Items!!!");
+                res.render("list", { listTitle: "Today", newListItems: foundItems, buttonName: "Work List🏢", redirectLocation: "/work" });
+            }
+        }
+    });
 });
 
-app.get("/work",function (req,res) { 
-    res.render("list",{listTitle:"Work List",newListItems:workItems,buttonName:"ToDo List✅",redirectLocation:"http://localhost:3000/"});
+app.get("/:customListName", function (req, res) {
+    var customListName =_.capitalize(req.params.customListName);
+    List.findOne({ name: customListName }, function (err, foundList) {
+        if (!err) {
+            if (!foundList) {
+                //Create a new List
+                var list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                //Show an exisiting List
+                res.render("list", { listTitle: foundList.name, newListItems: foundList.items, buttonName: "ToDo List✅", redirectLocation: "/" });
+            }
+        }
+    });
+
 });
 
-app.get("/about",function(req,res){
-    res.render("about",{listTitle:"About Us",redirectLocation:"http://localhost:3000/"});
-});
+// app.get("/work", function (req, res) {
+//     res.render("list", { listTitle: "Work List", newListItems: workItems, buttonName: "ToDo List✅", redirectLocation: "http://localhost:3000/" });
+// });
+
+// app.get("/about", function (req, res) {
+//     res.render("about", { listTitle: "About Us", redirectLocation: "http://localhost:3000/" });
+// });
 
 //Post Functions
-app.post("/",function(req,res){
-    var item = req.body.newItem;
-    if(req.body.list==="Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }else{
-        items.push(item);
+app.post("/", function (req, res) {
+    var itemName = req.body.newItem;
+    var listName = req.body.list;
+    var item = new Item({
+        name: itemName
+    });
+    if (listName === "Today") {
+        item.save();
         res.redirect("/");
+    } else {
+        List.findOne({ name: listName }, function (err, foundList) {
+            if (err) {
+                console.log(err);
+            } else {
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/" + listName);
+            }
+        });
     }
-    console.log(item);
 });
 
-app.post("/work",function(req,res){
-    var item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
-    console.log(item);
+app.post("/delete", function (req, res) {
+    var checkedItemId = req.body.checkbox;
+    var listName = req.body.listName;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function (err) {
+            if (!err) {
+                console.log("Removed Item from Default List");
+                res.redirect("/");
+            }
+        });
+    } else{
+        List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},function(err,foundList){
+            if(!err){
+                console.log("Removed Item from "+listName+" List");
+                res.redirect("/"+listName);
+            }
+        });
+    }
 });
 
 //Listen Functions
